915abfb199779f20f15cdec31d36f7f1eaee83e5
Ar-Ray-code <ray255ar@gmail.com>
2022-04-11 02:20:46 +0900

    update launch

--- motpy_ros/launch/example_mosaic.launch.py
+++ motpy_ros/launch/example_mosaic.launch.py
@@ -4,28 +4,28 @@ import launch_ros.actions
 def generate_launch_description():
 
     webcam = launch_ros.actions.Node(
-        package='v4l2_camera', node_executable='v4l2_camera_node',
+        package='v4l2_camera', executable='v4l2_camera_node',
         namespace='camera/color',
     )
     face_detect = launch_ros.actions.Node(
-        package='motpy_ros', node_executable='face_detect',
+        package='motpy_ros', executable='face_detect',
         parameters=[
             {'imshow_isshow' : 0}
         ]
     )
 
     tracking = launch_ros.actions.Node(
-        package='motpy_ros', node_executable='darknet_tracking'
+        package='motpy_ros', executable='darknet_tracking'
     )
 
     mosaic = launch_ros.actions.Node(
-        package='motpy_ros', node_executable='mosaic_bbox',
+        package='motpy_ros', executable='mosaic_bbox',
         remappings=[
             ('bounding_boxes','/tracking_data/bounding_boxes')
         ],
     )
     rqt_graph = launch_ros.actions.Node(
-        package='rqt_graph', node_executable='rqt_graph'
+        package='rqt_graph', executable='rqt_graph'
     )
 
     return launch.LaunchDescription([
